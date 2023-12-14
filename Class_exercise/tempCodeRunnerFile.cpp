8fb44c84dce4e8f1505667a374a25e3442532468
Nguyen Duc Phu <phunguyenduc99@gmail.com>
2023-12-14 10:07:50 +0700

    update

--- Class_exercise/tempCodeRunnerFile.cpp
+++ Class_exercise/tempCodeRunnerFile.cpp
@@ -9,18 +9,16 @@ int Y[N];	 // Y[k] is the next point of 0 on route k
 int X[N];	 // X[i] is the next point of i
 int load[N]; // load[k] is the amount of packages on route k
 int nbSegments;
+int nbRouteUsed;
 int f;			 // distance accumulated
 int Fmin;		 // min-distance found
 bool visited[N]; // visited[v] = true means that v is visited
-int sum = 0;
 void input()
 {
 	cin >> n >> K >> Q;
+
 	for (int i = 1; i <= n; i++)
-	{
 		cin >> d[i];
-		sum += d[i];
-	}
 	for (int i = 0; i <= n; i++)
 	{
 		for (int j = 0; j <= n; j++)
@@ -44,6 +42,7 @@ void printSolution()
 			cout << i << " ";
 		cout << "0, load = " << load[k] << endl;
 	}
+	cout << "nbRouteUsed = " << nbRouteUsed << " nbSegments = " << nbSegments << endl;
 }
 void solution()
 {
@@ -67,6 +66,12 @@ bool checkX(int v, int i, int k)
 }
 void TryX(int i, int k)
 { // find next point X[i] of i on route k
+	if (i == 0)
+	{
+		if (k < K)
+			TryX(Y[k + 1], k + 1);
+		return;
+	}
 	for (int v = 0; v <= n; v++)
 	{
 		if (checkX(v, i, k))
@@ -79,16 +84,20 @@ void TryX(int i, int k)
 			if (v == 0)
 			{ // return to the depot
 				if (k < K)
-					TryX(Y[k + 1], k + 1);
+				{
+					if (f < Fmin)
+						TryX(Y[k + 1], k + 1);
+				}
 				else
 				{
-					if (nbSegments == n + K)
+					if (nbSegments == nbRouteUsed + n)
 						solution();
 				}
 			}
 			else
 			{
-				TryX(v, k);
+				if (Fmin > f)
+					TryX(v, k);
 			}
 			visited[v] = false;
 			f = f - c[i][v];
@@ -99,6 +108,8 @@ void TryX(int i, int k)
 }
 bool checkY(int v, int k)
 {
+	if (v == 0)
+		return true;
 	if (visited[v])
 		return false;
 	if (load[k] + d[v] > Q)
@@ -107,7 +118,8 @@ bool checkY(int v, int k)
 }
 void TryY(int k)
 { // try values for Y[k]
-	for (int v = Y[k - 1] + 1; v <= n - K + k; v++)
+	// for(int v = Y[k-1]+1; v <= n-K+k; v++){
+	for (int v = (Y[k - 1] == 0 ? 0 : Y[k - 1] + 1); v <= n; v++)
 	{
 		if (checkY(v, k))
 		{
@@ -115,41 +127,41 @@ void TryY(int k)
 			visited[v] = true;
 			f = f + c[0][v];
 			load[k] += d[v];
-			nbSegments += 1;
+			if (v > 0)
+				nbSegments += 1;
 			if (k == K)
 			{
-				TryX(Y[1], 1);
+				nbRouteUsed = nbSegments;
+				if (f < Fmin)
+				{
+					TryX(Y[1], 1);
+				}
 			}
 			else
 			{
-				TryY(k + 1);
+				if (Fmin > f)
+					TryY(k + 1);
 			}
 			visited[v] = false;
 			f = f - c[0][v];
 			load[k] -= d[v];
-			nbSegments -= 1;
+			if (v > 0)
+				nbSegments -= 1;
 		}
 	}
 }
 int main()
 {
-	ios_base::sync_with_stdio(0);
-	cin.tie(NULL);
-	cout.tie(NULL);
 	input();
-	// for (int v = 1; v <= n; v++)
-	// 	visited[v] = false;
-	// for (int k = 1; k <= K; k++)
-	// 	load[k] = 0;
+	for (int v = 1; v <= n; v++)
+		visited[v] = false;
+	for (int k = 1; k <= K; k++)
+		load[k] = 0;
 	f = 0;
 	Fmin = 1e9;
 	nbSegments = 0;
 	Y[0] = 0;
-	while (K > ceil(sum / Q))
-	{
-		TryY(1);
-		K--;
-	}
+	TryY(1);
 	cout << Fmin;
 	return 0;
 }
---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,174 @@
+#include <bits/stdc++.h>
+#define N 100
+using namespace std;
+
+int n, K, q;
+int d[N];
+int c[N][N];
+int Fmin = INT_MAX;
+int f = 0;
+int segment = 0; // segment = n+(so xe su dung) => di qua tat ca cac diem va quay ve 0
+
+int x[N];    // diem tiep theo cua khach hang i (tong cong n khach hang)
+int y[N];    // diem khoi dau cua xe thu i tu diem 0 (tong cong k xe, co the dung < k xe)
+int load[N]; // khoi luong mang theo cua xe i
+bool visited[N];
+
+int routeUsed = 0;
+
+void checkinput()
+{
+    cout << n << endl
+         << K << endl
+         << q << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        cout << d[i] << " ";
+    }
+    cout << endl;
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            cout << c[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void input()
+{
+    cin >> n >> K >> q;
+    for (int i = 1; i <= n; i++)
+    {
+        cin >> d[i];
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            cin >> c[i][j];
+        }
+    }
+}
+
+void solution()
+{
+    Fmin = min(Fmin, f);
+}
+
+bool checkX(int i, int k, int v)
+{
+    if (v == 0)
+        return true;
+    if (visited[v])
+        return false;
+    if (load[k] + d[v] > q)
+        return false;
+    return true;
+}
+
+void TryX(int i, int k) // tim duong tiep theo sai i (a.k.a x[i]), route cua xe k
+{
+    if (i > 0)
+    {
+        for (int v = 0; v <= n; v++)
+        {
+            if (checkX(i, k, v))
+            {
+                x[i] = v;
+                f += c[i][v];
+                load[k] += d[v];
+                visited[v] = true;
+                segment++;
+                if (v == 0)
+                { // return to the depot
+                    if (k < K)
+                    {
+                        if (f < Fmin)
+                            TryX(y[k + 1], k + 1);
+                    }
+                    else
+                    {
+                        if (segment == routeUsed + n)
+                            solution();
+                    }
+                }
+                else
+                {
+                    if (Fmin > f)
+                        TryX(v, k);
+                }
+
+                f -= c[i][v];
+                load[k] -= d[v];
+                segment--;
+                visited[v] = false;
+            }
+        }
+    }
+    else if (k < K)
+        TryX(y[k + 1], k + 1);
+}
+
+bool checkY(int k, int v)
+{
+    if (v == 0)
+        return true;
+    if (visited[v])
+        return false;
+    if (load[k] + d[v] > q)
+        return false;
+    return true;
+}
+
+void TryY(int k)
+{
+    for (int v = (y[k - 1] == 0 ? 0 : (y[k - 1] + 1)); v <= n; v++) // vi khoi luong moi xe nhu nhau nen gia su y[1] < y [2] ...
+    {
+        if (checkY(k, v))
+        {
+            y[k] = v;
+            load[k] += d[v];
+            f += c[0][v];
+            visited[v] = true;
+            if (v > 0)
+            {
+                segment++;
+            }
+            if (f < Fmin)
+            {
+                if (k == K)
+                {
+                    routeUsed = segment;
+                    TryX(y[1], 1);
+                }
+                else
+                {
+                    TryY(k + 1);
+                }
+            }
+            load[k] -= d[v];
+            visited[v] = false;
+            f -= c[0][v];
+            if (v > 0)
+            {
+                segment--;
+            }
+        }
+    }
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    freopen("input.txt", "r", stdin);
+    input();
+
+    y[0] = 0;
+    TryY(1);
+    cout << Fmin << endl;
+    return 0;
+}
